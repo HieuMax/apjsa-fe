405e1289faeda3ad14d9f0f1a9fc2e8bbc566298
HieuMax <lloc45723@gmail.com>
2025-06-25 11:29:05 +0700

    APJSA-15 Login

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+export const API_URL = "https://gettech-production.up.railway.app";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
